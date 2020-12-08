7903e89fd2e49d616c1133ba15647415f461629a
Luiz Cezer <lccezinha@gmail.com>
2020-12-07 21:48:46 -0300

    Basic test for teacher controller

---
+++ test/you_speak_web/controllers/teachers/registration_controller_test.exs
@@ -0,0 +1,28 @@
+defmodule YouSpeakWeb.Teachers.RegistrationControllerTest do
+  use YouSpeakWeb.ConnCase
+
+  alias YouSpeak.Factory
+
+  def user_factory, do: Factory.insert!(:user)
+  def teacher_factory(attributes), do: Factory.insert!(:teacher, attributes)
+
+  # @valid_params %{title: "My Title"}
+  # @invalid_params %{title: ""}
+
+  setup %{conn: conn} do
+    user_data = user_factory()
+
+    conn =
+      conn
+      |> Plug.Test.init_test_session(user_id: user_data.id)
+      |> YouSpeakWeb.Plugs.SetUser.call(%{})
+
+    {:ok, conn: conn}
+  end
+
+  test "GET /teacher/registrarion/new", %{conn: conn} do
+    conn = get(conn, Routes.registration_path(conn, :new))
+
+    assert html_response(conn, 200) =~ "RegistrationController#new"
+  end
+end
---
+++ test/you_speak_web/views/teachers/registration_view.ex
@@ -0,0 +1,3 @@
+defmodule YouSpeakWeb.RegistrationViewTest do
+  use YouSpeakWeb.ConnCase, async: true
+end
