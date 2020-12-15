165d5ed9c3d15d40e4c017271bb250e999783f90
Luiz Cezer <lccezinha@gmail.com>
2020-12-14 23:02:48 -0300

    Fix typo

--- lib/you_speak/teachers/use_cases/find_by_user_id.ex
+++ lib/you_speak/teachers/use_cases/find_by_user_id.ex
@@ -1,6 +1,6 @@
 defmodule YouSpeak.Teachers.UseCases.FindByUserID do
   @moduledoc """
-  FindByUserID return a teacher based on the user_id.
+  FindByUserID return a teacher based on the user_id or raises and error.
   """
 
   alias YouSpeak.Teachers.Schemas.Teacher
