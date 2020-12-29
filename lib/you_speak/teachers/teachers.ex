a97e1e883b04d7b01398ddd5416bb60370168b0b
Luiz Cezer <lccezinha@gmail.com>
2020-12-28 22:47:40 -0300

    Mix format

--- lib/you_speak/teachers/teachers.ex
+++ lib/you_speak/teachers/teachers.ex
@@ -3,7 +3,8 @@ defmodule YouSpeak.Teachers do
   Teachers bounded context contains all the use cases available to the Teachers context.
   """
 
-  @spec registration(map()) :: YouSpeak.Teachers.UseCases.Registration.ok_teacher_or_error_changeset
+  @spec registration(map()) ::
+          YouSpeak.Teachers.UseCases.Registration.ok_teacher_or_error_changeset()
   def registration(params), do: YouSpeak.Teachers.UseCases.Registration.call(params)
 
   @spec find_by_user_id(integer()) :: YouSpeak.Teachers.Schemas.Teacher | nil
