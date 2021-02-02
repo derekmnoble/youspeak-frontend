6e9e3fab3801a581e4b97bd61b84733740218ba8
Luiz Cezer <lccezinha@gmail.com>
2021-02-01 23:23:27 -0300

    Add group and list by logged teacher

--- test/you_speak_web/controllers/groups/group_controller_test.exs
+++ test/you_speak_web/controllers/groups/group_controller_test.exs
@@ -6,16 +6,16 @@ defmodule YouSpeakWeb.Groups.GroupControllerTest do
   def user_factory, do: Factory.insert!(:user)
   def teacher_factory(attributes), do: Factory.insert!(:teacher, attributes)
 
-  # @valid_params %{
-  #   name: "Name",
-  #   description: "description"
-  # }
-  # @invalid_params %{
-  #   name: "",
-  #   namespace: "namespace",
-  #   description: "",
-  #   url: ""
-  # }
+  @valid_params %{
+    name: "Name",
+    description: "description"
+  }
+  @invalid_params %{
+    name: "",
+    namespace: "namespace",
+    description: "",
+    url: ""
+  }
 
   setup %{conn: conn} do
     user = user_factory()
@@ -41,17 +41,18 @@ defmodule YouSpeakWeb.Groups.GroupControllerTest do
     assert html_response(conn, 200) =~ "Add group"
   end
 
-  # describe "POST /groups" do
-  #   test "with valid data must create a new group and redirect to page path", %{conn: conn} do
-  #     conn = post(conn, Routes.group_path(conn, :create), group: @valid_params)
-  #
-  #     assert get_flash(conn, :info) == "Group created!"
-  #     assert redirected_to(conn) == Routes.page_path(conn, :index)
-  #   end
-  # test "with invalid data render errors and keep in the new page", %{conn: conn} do
-  #     conn = post(conn, Routes.group_path(conn, :create), group: @invalid_params)
-  #
-  #     assert html_response(conn, 200) =~ "Add group"
-  #   end
-  # end
+  describe "POST /groups" do
+    test "with valid data must create a new group and redirect to page path", %{conn: conn} do
+      conn = post(conn, Routes.group_path(conn, :create), group: @valid_params)
+
+      assert get_flash(conn, :info) == "Group created!"
+      assert redirected_to(conn) == Routes.group_path(conn, :index)
+    end
+
+    test "with invalid data render errors and keep in the new page", %{conn: conn} do
+      conn = post(conn, Routes.group_path(conn, :create), group: @invalid_params)
+
+      assert html_response(conn, 200) =~ "Add group"
+    end
+  end
 end
