b61104313f61a6dc432cef25456bae0c977022df
Luiz Cezer <lccezinha@gmail.com>
2021-02-16 23:37:21 -0300

    Add table meetings

---
+++ priv/repo/migrations/20210217023510_create_meetings.exs
@@ -0,0 +1,15 @@
+defmodule YouSpeak.Repo.Migrations.CreateMeetings do
+  use Ecto.Migration
+
+  def change do
+    create table(:meetings) do
+      add :title, :string, null: false, limit: 200
+      add :description, :text
+      add :video_url, :string, null: false
+
+      add :group_id, references(:groups)
+
+      timestamps()
+    end
+  end
+end
