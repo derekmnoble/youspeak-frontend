16e1d1ce607596e0f3344d290ae6ef8efcb764aa
Luiz Cezer <lccezinha@gmail.com>
2021-03-08 23:35:29 -0300

    Expose list_by_group_slug use case

--- lib/you_speak/meetings/meetings.ex
+++ lib/you_speak/meetings/meetings.ex
@@ -3,8 +3,11 @@ defmodule YouSpeak.Meetings do
   Meetings bounded context contains all use cases available to the Meetings context.
   """
 
-  alias YouSpeak.Meetings.UseCases.{Create}
+  alias YouSpeak.Meetings.UseCases.{Create, ListByGroupSlug}
 
   @spec create(Create.params()) :: Create.ok_meeting_or_error_changeset()
   def create(params), do: Create.call(params)
+
+  @spec list_by_group_slug(ListByGroupSlug.params()) :: ListByGroupSlug.meetings_or_empty()
+  def list_by_group_slug(params), do: ListByGroupSlug.call(params)
 end
