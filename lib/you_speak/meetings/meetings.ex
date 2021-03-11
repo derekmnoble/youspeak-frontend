a0e01d0188a4ad74a73a669524400cbacbb871c0
Luiz Cezer <lccezinha@gmail.com>
2021-03-10 22:50:13 -0300

    Add Meeting.GetBySlug use case

--- lib/you_speak/meetings/meetings.ex
+++ lib/you_speak/meetings/meetings.ex
@@ -3,11 +3,14 @@ defmodule YouSpeak.Meetings do
   Meetings bounded context contains all use cases available to the Meetings context.
   """
 
-  alias YouSpeak.Meetings.UseCases.{Create, ListByGroupSlug}
+  alias YouSpeak.Meetings.UseCases.{Create, ListByGroupSlug, GetBySlug}
 
   @spec create(Create.params()) :: Create.ok_meeting_or_error_changeset()
   def create(params), do: Create.call(params)
 
   @spec list_by_group_slug(ListByGroupSlug.params()) :: ListByGroupSlug.meetings_or_empty()
   def list_by_group_slug(params), do: ListByGroupSlug.call(params)
+
+  @spec get_by_slug!(GetBySlug.params()) :: Get.meeting_or_exception()
+  def get_by_slug!(params), do: GetBySlug.call(params)
 end
